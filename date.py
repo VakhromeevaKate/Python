8bba07e8d1895a4eb6bf678f84a653c73986aca7
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-02-17 22:10:57 +0300

    Paul Barry Python lessons: The 1st

--- date.py
+++ date.py
@@ -1,6 +1,10 @@
 import datetime
+import time
 
 print(datetime.date.today())
 print(datetime.date.today().day)
 print(datetime.date.today().month)
 print(datetime.date.today().year)
+
+
+print(time.strftime("%I:%M %p %A"))
---
+++ html_escape.py
@@ -0,0 +1,3 @@
+import html
+print(html.escape("This HTML contains a <script>script</script> tag"))
+print(html.unescape("I &hearts; Python's &lt;standart library&gt;."))
