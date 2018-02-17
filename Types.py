7749179538cd22183b229cc9dbf2d8ab648e7682
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-02-17 20:04:17 +0300

    types playground - fractions

---
+++ Types.py
@@ -0,0 +1,14 @@
+from fractions import Fraction
+
+print(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1)
+
+# uncomment the row below - this is an example of float exceeded
+a = 3 ** 1000
+# a += 0.1
+print(a)
+
+print(Fraction(' 3/7 ') + Fraction('1/3') + Fraction(1, 3))
+
+x = complex(1, 2)
+print(x)
+
