a4178fe720d3a634887558732234387164485b79
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-02-23 22:53:31 +0300

    dictionary example 1

--- vowels.py
+++ vowels.py
@@ -1,8 +1,9 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
 word = input("Provide a word to search vowels: ")
-found = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+found = {}
 for letter in word:
     if letter in vowels:
+        found.setdefault(letter, 0)
         found[letter] += 1
 for k, v in sorted(found.items()):
     print(k, "was found", found[k], "times(s)")
